b65724985521af8ccd966d1f8d1bce1cf29265d2
Stephen Osserman <stephen.osserman@gmail.com>
2022-10-10 10:52:23 -0700

    updating text transition, changing timeline years flex justify and adding 1776

--- abortion-make-america-what-again/option2/scripts.js
+++ abortion-make-america-what-again/option2/scripts.js
@@ -3,7 +3,9 @@ const timelineText = d3.select('.timeline-text');
 
 const viewportWidth = window.innerWidth;
 const numSteps = timeline.node().children.length;
-const totalLength = numSteps * viewportWidth; 
+const stepOverlapFactor = 0.7; // this lets the next year in the timeline show before the last one is totally gone. 
+//const textDelayFactor = 0.05; // this is how far to scroll before first text changes. 
+const totalLength = (/*textDelayFactor + */numSteps) * viewportWidth * stepOverlapFactor;  
 const startTopPos = parseInt(timeline.style('top'));
 
 const scrollScale = d3.scaleLinear().domain([0, 100]).range([0, numSteps]);
@@ -11,16 +13,18 @@ const topScale = d3.scaleLinear().domain([100, 120]).range([startTopPos, 50]);
 
 let heightOffset = 0;
 let lastScroll = 0;
+let lastscrollIndex;
 let scrollPercent = 0;
 
 const textArray = [
-  "2022: 6 MAGA Justices overtuned Roe, took away the right to abortions for millions of Americans and turned the clock back to 1972. But they aren't stopping there",
+  "2022: <b>6 MAGA Justices overtuned Roe</b>, took away the right to abortions for millions of Americans and turned the clock back to 1972. But they aren't stopping there",
   "After Roe was overturned, Michigan Republicans are trying to enforce a 1930s anti-abortion law —from a time when there was no right to contraception, same-sex relations were a felony, interracial marriage was illegal, and pregnancy was a fireable offense.",
   "... MAGA Republicans are trying to enforce some of the country's oldest abortion laws — from the 1800s — in West Virginia, Wisconsin and Arizona and elsewhere.  These laws date back to a time when only some white men could vote and enslaving people was still legal across the U.S. South.",
   "MAGA Justices say they look back to the original intent of the \"founders\"— white men who allowed slavery and lived at a time when women had to forfeit property when they married. And even then, they did not ban abortions in America's founding.",
   "In overturning of Roe the MAGA Justices even quotes an English judge from the 1600s—one also wrote justifying rape in marriage and sentenced two elderly women to death for witchcraft in 1662.",
   "But we've defeated the MAGAs of previous generations before.",
-  "Generations of abolitionists freed individual slaves and helped end slavery.",
+  "From before the country's founding, women took care of eachother. Midwife-provided abortion was legal, safe and common.",
+  "Abolitionists built an underground railroad and then helped end slavery.",
   "Suffragists helped secure voting for white women. And workers came together to secure minimum wage, workers rights, and a 2-day weekend.",
   "The Civil Rights movement expanded more rights to Black people through its fight for political and economic power. And activists won LGBTQ+ rights including marriage equality.",
   "Together, we moved beyond the judges of the 1600s and the laws of the 1800s and made progress toward freedom and equal rights. And we'll do it again.",
@@ -48,10 +52,13 @@ function updateText(text) {
   d3.select('.timeline-text span').transition()
     .duration(300)
     .style('opacity', 0)
-    .transition()
-    .duration(500)
-    .text(text)
-    .style('opacity', 1);
+    .on('end',() => {
+      d3.select('.timeline-text span')
+      .html(text)
+      .transition()
+      .duration(500)
+      .style('opacity', 1);
+  })
 }
 
 function onScroll() {
@@ -60,13 +67,15 @@ function onScroll() {
 
   //calculate scroll progress
   scrollPercent = Math.round(window.scrollY / totalLength * 100);
-
+  console.log('scrollPercent', scrollPercent)
   //calculate scroll index
-  const scrollIndex = (scrollDir==='down') ? scrollScale(scrollPercent) : scrollScale(scrollPercent)-1;
+  const scrollIndex = Math.floor((scrollDir==='down') ? scrollScale(scrollPercent) : (scrollScale(scrollPercent)-1));
+  console.log('scrollIndex', scrollIndex)
 
   //update timeline text
   const text = textArray[scrollIndex];
-  if (text!==undefined) updateText(text);
+  if (lastscrollIndex!==scrollIndex) updateText(text);
+  //if (text!==undefined ) updateText(text);
 
   //set position of timeline
   if (scrollPercent>=100 && scrollPercent<=120) {
@@ -74,12 +83,13 @@ function onScroll() {
     timeline.style('top', `${newTopPos}px`);
   }
   else {
-    let newPos = (scrollPercent<100) ? -(totalLength-window.scrollY) : totalLength-window.scrollY+viewportWidth;
+    let newPos = (scrollPercent<100) ? -(totalLength-window.scrollY) : totalLength-window.scrollY+(viewportWidth*stepOverlapFactor);
     timeline.style('left', `${newPos}px`);
   }
 
   //save last scroll position
   lastScroll = window.scrollY;
+  lastscrollIndex = scrollIndex;
 }
 
 
