3fe0192a87f5ece84ac6d5d4d7afa84b5c723ed8
Stephen Osserman <stephen.osserman@gmail.com>
2022-10-18 13:40:54 -0700

    final polishing, cleaning code, etc

--- abortion-make-america-what-again/option3/scripts.js
+++ abortion-make-america-what-again/option3/scripts.js
@@ -1,3 +1,4 @@
+// select core elements and variables. 
 const timeline = d3.select('.timeline');
 const timelineImg = d3.select('.timeline-img');
 const timelineText = d3.select('.timeline-text');
@@ -7,39 +8,30 @@ const viewportWidth = window.innerWidth;
 const mobileStyle = viewportWidth < 576;
 const timelineVerticalPadding = mobileStyle ? 0 : 50 ;
 
-let scrollableHeight = 0;
-let heightOffset = 0;
-let lastScroll = 0;
-let lasttimelineStepIndex;
-let scrollPercent = 0;
-
 const timelineStepArray = [
   {type: 'intro',  yearPos: 5},
-  {type: 'scene', align: 'start', yearPos: 4, text: "<h3 class='red'><span>6 MAGA Justices overturned Roe v. Wade</span></h3><p>They took away the right to abortions for millions of Americans and turned the clock back to 1972. But they aren't stopping there.", img: "img/AbortionTimeline2.jpg"},
-  {type: 'scene',align: 'start', yearPos: 3, text: "<h3 class='red'><span>They unleashed laws from the 1930's</span></h3><p>Michigan Republicans are trying to enforce a 1930s anti-abortion law &mdash;from a time when there was no right to contraception, same-sex relations were a felony, interracial marriage was illegal, and pregnancy was a fireable offense.</p>", img:  "img/1930R.png"},
-  {type: 'scene', align: 'start', yearPos: 2, text: "<h3 class='red'><span>MAGA is even trying to enforce laws from 1800s</span></h3><p>In West Virginia, Wisconsin and Arizona, Republicans are trying to enforce some of the country's oldest anti-abortion laws, which date back to a time when only some white men could vote and enslaving people was still legal across the U.S. South.</p>", img: "img/AbortionTimeline4.jpg"}, 
-  {type: 'scene', align: 'start', yearPos: 1, text: "<h3 class='red'><span>They look back to the 1700s</span></h3><p>MAGA Justices argue they look back to the original intent of the \"founders\"&mdash; white men who allowed slavery and lived at a time when women had to forfeit property when they married. And even then, they did not ban abortions in America's founding.</p>", img: "img/AbortionTimeline5.jpg"},
-  {type: 'scene', align: 'start', yearPos: 0, text: "<h3 class='red'><span>And even earlier</span></h3><p>In overturning of Roe the MAGA Justices even quotes an English judge from the 1600s&mdash;who also wrote justifying rape in marriage and sentenced two elderly women to death for witchcraft in 1662</p>.", img: "img/AbortionTimeline6.jpg"},
-  {type: 'scene', align: 'start', yearPos: 0, pivot: true, text: "<h3 class='blue'><span>But we've defeated the MAGAs of previous generations before.</span></h3>", img: "img/AbortionTimeline6.jpg"},
-  {type: 'scene', align: 'end', yearPos: 1, text: "<h3 class='blue'><span>We've taken care of each other</span></h3><p>From before the country's founding, Midwife-provided abortion was legal, safe and common.", img:"img/AbortionTimeline7.jpg"},
-  {type: 'scene', align: 'end', yearPos: 2, text: "<h3 class='blue'><span>Abolitionists built an underground railroad</span></h3><p>And then helped end slavery.", img: "img/AbortionTimeline8.jpg"},
-  {type: 'scene', align: 'end', yearPos: 3, text: "<h3 class='blue'><span>Suffragists and Workers Won Rights</span></h3><p>Suffragists helped secure voting for white women. And workers came together to secure minimum wage, workers' rights, and a two-day weekend</p>.", img: "img/AbortionTimeline9.jpg"},
-  {type: 'scene', align: 'end', yearPos: 4, text: "<h3 class='blue'><span>Activists expanded those rights</span></h3><p>The Civil Rights movement expanded more rights to Black people through the fight for political and economic power. And activists won LGBTQ+ rights, including marriage equality.", img: "img/AbortionTimeline10.jpg"},
-  {type: 'scene', align: 'end', yearPos: 5, text: "<h3 class='blue'><span>We've made progress before...</span></h3>", img: 'img/AbortionTimeline11.jpg'},
-  {type: 'scene', align: 'end', yearPos: 6, text: "<h3 class='blue'><span>November 8th <br/> we do it again!</span></h3>", img: 'img/AbortionTimeline11.jpg'},
-  //{type: 'scene', align: 'end', yearPos: 5, text: "<h3 class='blue'><span>We've made progress before...</span></h3><p>Together, we moved beyond the bigoted judges of the 1600s and the laws of the 1800s and made progress toward freedom and equal rights.</p> ", img: 'img/AbortionTimeline11.jpg'},
+  {type: 'scene', align: 'start', yearPos: 4, img: "img/AbortionTimeline2.jpg"},
+  {type: 'scene',align: 'start', yearPos: 3, img:  "img/AbortionTimeline3.jpg"},
+  {type: 'scene', align: 'start', yearPos: 2, img: "img/AbortionTimeline4.jpg"}, 
+  {type: 'scene', align: 'start', yearPos: 1, img: "img/AbortionTimeline5.jpg"},
+  {type: 'scene', align: 'start', yearPos: 0, img: "img/AbortionTimeline6.jpg"},
+  {type: 'scene', align: 'start', yearPos: 0, pivot: true,  img: "img/AbortionTimeline6.jpg"},
+  {type: 'scene', align: 'end', yearPos: 1, img:"img/AbortionTimeline7.jpg"},
+  {type: 'scene', align: 'end', yearPos: 2, img: "img/AbortionTimeline8.jpg"},
+  {type: 'scene', align: 'end', yearPos: 3, img: "img/AbortionTimeline9.jpg"},
+  {type: 'scene', align: 'end', yearPos: 4, img: "img/AbortionTimeline10.jpg"},
+  {type: 'scene', align: 'end', yearPos: 5, img: 'img/AbortionTimeline11.jpg'},
+  {type: 'scene', align: 'end', yearPos: 6, img: 'img/AbortionTimeline11.jpg'},
 ];
 
 // preload the images
 let images = new Array()
 let i = 0;
 timelineStepArray.forEach(d=> { 
-  console.log('loaded ' + d.img)
   if(d.img != undefined){
     images[i] = new Image()
     images[i].src = d.img
   }
-
 })
 
 const totalPageSteps = timelineStepArray.length; 
@@ -48,12 +40,6 @@ function initTimeline() {
   //set up dimensions and position of timeline
   timeline.style('width', `600vw`);
   timeline.style('left', mobileStyle ? `-520vw`: `-600vw`);
-
-  //set height of content to "scroll" through (we're scrolling back and forth through the timeline plus one screen where the timeline moves up and the intro and outro screens)
-  timelineText.selectAll('div')
-    .data(timelineStepArray)
-    .join('div')
-    .html(d=> d.text)
 }
 
 let curStep = 0; 
@@ -61,44 +47,30 @@ let isMoving = 0;
 
 updateImg = function(step){
   if(step <= 0) {
-    timelineImg.style('background-image', `url(img/2022R.jpeg)`);
+    timelineImg.style('background-image', `url(img/AbortionTimeline1.jpg)`);
   } else  {
-    console.log('step', step, ', img: ', timelineStepArray[step].img)
     timelineImg.style('background-image', `url(${timelineStepArray[step].img})`);
   }
 }  
 
 updateTexts = function(step, isNext){
-  d3.selectAll('.timeline-text div:nth-child(' +(step+1) +')')
+  d3.selectAll('.timeline-text>div:nth-child(' +(step+1) +')')
     .style('top',isNext ? '100%' : '-90%') // this is a bug - not sure why I have to set 100% when set in css. It's for some reason starting way off from this
-    //.style('scale',0.5)
     .transition()
     .duration(1100)
     .delay(150)// time for image to start changing first
-    .style('top',  step < 6 ? (mobileStyle ? "0%" :'10%') : (mobileStyle || step == (totalPageSteps-1) ? "15%" :'40%'))
+    .style('top',  step < 6 ? (mobileStyle ? "15%" :'10%') : (mobileStyle || step == (totalPageSteps-1) ? "25%" :'40%'))
     .style('scale',1)
     .style('opacity',1)
     .on('end', () => {
       isMoving = 0
-      /* 
-      if( step == (totalPageSteps-1) ){
-        d3.selectAll('.timeline-text div:nth-child(' +(step+1) +')')
-          .style('right', '10%')
-          .transition().duration(1100).delay(1500)
-          .style('right', '100%')
-          .on('end', () => {
-            d3.select('.cta').style('display','flex').transition().duration(600).style('opacity', 1)
-
-          })
-      }*/
     })
   if((curStep > 0 & isNext) || (curStep < totalPageSteps & !isNext)){
-    d3.selectAll('.timeline-text div:nth-child(' +(step + (isNext ? 0 : +2))+')')
+    d3.selectAll('.timeline-text>div:nth-child(' +(step + (isNext ? 0 : +2))+')')
       .transition()
       .delay(150)// time for image to changing first
       .duration(1100)
       .style('top',isNext ? '-90%' : '100%')
-      //.style('scale',0.5)
       .style('opacity',0)
       .on('end', () => {
         isMoving = 0 // need to set it here for when moving backwards
@@ -167,17 +139,17 @@ updateScene = function(direction){
     d3.select('.timeline').style('opacity', 0);
   }
   if(curStep == (totalPageSteps-1)){
-    d3.select('.cta').style('display','flex').transition().delay(1000).duration(600).style('opacity', 1)
+    d3.select('.cta').style('display','flex').transition().delay(1400).duration(500).style('opacity', 1)
+    
   } else { 
-    d3.select('.cta').style('display','none').transition().duration(1000).style('opacity', 0)
+    d3.select('.cta').style('display','none').style('opacity', 0)
   }
 
   updateImg(curStep)
-  
 }
 
 d3.select('.cta .share-btn').on('click', ()=>{
-  isMoving =1;
+  isMoving =1; // just to keep people from navigating through background when overlay is open
   d3.select('section.overlay').style('display','block')
   d3.select('#overlay-background').style('display','block')
 })
@@ -211,15 +183,6 @@ window.addEventListener('touchmove', (e) => {
 });
 window.addEventListener('touchend', (e)=> { touchStartY = null});
 
-
-
-/* d3.select('html').on('click', (e) => { 
-  if (e.clientY > window.innerHeight/2){
-    updateScene('next')
-  } else {
-    updateScene('previous')
-  }
-})*/
 
 d3.select('html').on("keydown", (e)=> {
   if (e.keyCode == 40){
