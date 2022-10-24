8112214a9337d93547e05153d7a3437bf3cef131
Stephen Osserman <stephen.osserman@gmail.com>
2022-10-24 12:54:49 -0700

    styling updates and sparkling bubbles

--- abortion_we_are_viz/script.js
+++ abortion_we_are_viz/script.js
@@ -20,21 +20,14 @@ const initialOffsets = d3.cumsum(texts.map((d,i)=> (smallerScreen ? mapTopToScre
 const maxOffset = d3.max(initialOffsets)
 console.log(mapTopToScreenBottomVh, initialOffsets, maxOffset)
 
-const bubblesIn = function(x) { 
-    const [start,end] = [0.1,0.2]
-    if (x < start) return 0 
-    if (x > end) return 1 
-    let scale = d3.scaleLinear().domain([start,end]).range([0,1])
-    return scale(x) 
-}
-
 d3.select('.wrapper').style('top', 100*mapTopVh + 'vh')
 d3.select('#full-height-for-scroll').style('height', 100*(1 + maxOffset) + "vh")
 
 const svg = d3.select('.wrapper>svg')
 const mapG = svg.append('g').attr('fill','none').attr('stroke','#000')
     .attr('stroke-linejoin','round').attr('stroke-linecap','round')
-const bubblesG = svg.append('g').attr('fill','brown')
+const bubblesG = svg.append('g').attr('fill','#A52A2A66').attr('id', 'bubbles-g')
+const sparklesG = svg.append('g')
 const textsDiv = d3.select('.wrapper>div#scrolling-texts')
 const weVote = d3.select('.wrapper>#we-vote') 
 
@@ -43,17 +36,17 @@ textsDiv.selectAll('div').data(texts).join('div')
     .style('top',(d,i)=> smallerScreen ? (100 * initialOffsets[i] + "%")  : (100 * (d.top + initialOffsets[i]) ) + '%')
     .style('left',d=> smallerScreen ? "5%" : 100*d.left + '%').style('right',d=> d.right * 100  + '%')
     .style('text-align', (d,i)=> i==0 ? 'right' : 'left')
-    .style('color', (d,i)=> i==0 ? 'black' : 'brown')
-    .style('background-color', (d,i)=> i==0 ? 'revert' : '#FFFFFFAA')
     .style('padding', '4px')
     .style('border-radius', '4px')
 
+let bubbles; 
 d3.json('abortion-data-we-are-the.json')
     .then( function (d) {
     d3.select('.scroll-arrow').style('opacity', 1)
     const nationPath = d.nationPath;
     const statesPath = d.statesPath; 
-    const bubbles = d.bubbles; 
+    bubbles = d.bubbles; 
+    console.log('bubbles.length', bubbles.length)
     mapG.append('path')
         .attr('fill','#eee')
         .attr('stroke','#aaa')
@@ -68,11 +61,48 @@ d3.json('abortion-data-we-are-the.json')
         .join('circle')
         .attr("transform", d=> `translate(${d.coords})`)
         .attr("r", d => d.radius)
-        .attr('fill-opacity',0.4)
+        //.attr('fill-opacity',0.4) // now assigning this as part of fill color with 66 at end of hex
         .attr('stroke','white')
         .attr('stroke-width',0.5)
         .attr('stroke-opacity',1)
+    
     });
+
+let isSparkling = 0; 
+sparkleMap = function() {
+    isSparkling = 1;
+    if(bubbles != undefined ){
+
+        /* this version I tried after the initial sparkle version did a throb effect... but I didn't really like it either 
+        bubblesG.transition()
+            .attr('fill', '#A52A2ACC')
+            .on('end', () => {
+                bubblesG.transition()
+                    .attr('fill', '#A52A2A66')
+                    .on('end', () => {isSparkling = 0 })
+                })
+        return ;
+        */ 
+        const curBubbles = d3.shuffle(bubbles.filter(d=> Math.random()< 0.16))
+        const curSparkles = sparklesG.selectAll('circle')
+            .data(curBubbles)
+            .join('circle')
+            .attr("r", 2.5)
+            .attr('fill','yellow')
+            .attr("transform", d=> `translate(${d.coords})`)
+            .attr('opacity', 0)
+        curSparkles.transition()
+            .duration(200)
+            .delay((d,i) => i* 4)
+            .attr('opacity',1)
+        curSparkles.transition()
+            .delay((d,i) => 200 + i* 4)
+            .duration(200)
+            .attr('opacity',0)
+            .on('end', () => {isSparkling = 0})
+    }
+}
+svg.on('click', sparkleMap)
 
 let goingBlue = false;
 let hideArrow = false
@@ -96,8 +126,9 @@ update = function(frame) {
         d3.select('.scroll-arrow').transition().style('opacity',0)
     }
     
+    d3.select('footer').classed('inactive', frame > 0)
 
-    bubblesG.attr('opacity',bubblesIn(frame))
+    bubblesG.classed('active', frame > 0.01)
 
     let textFrame = d3.min([1,frame/finishTextsFrame]);
     textsDiv
@@ -106,22 +137,30 @@ update = function(frame) {
                 d3.max([(smallerScreen ? -3 : d.top ), 
                         (smallerScreen ? 0 : d.top ) + initialOffsets[i] - (textFrame* maxOffset) ]
                     ) * 100 + '%'))
-        .style('background-color', (d,i)=> (i==0 | initialOffsets[i] <= (textFrame * maxOffset) ? 'revert' : '#FFFFFFAA' ) )
-    
+        .classed('inactive',(d,i)=> (initialOffsets[i] <= (textFrame * maxOffset) ))
+        .each(function(d,i) {
+            if (i> 0 & !d3.select(this).classed("alreadySparkled") & !isSparkling){
+                if ( (smallerScreen ? 0 : d.top ) + initialOffsets[i] - (textFrame* maxOffset) < .9 ){
+                    d3.select(this).classed('alreadySparkled',true)
+                    sparkleMap(); 
+                }
+            }
+        })
+        //.style('background-color', (d,i)=> (i==0 | initialOffsets[i] <= (textFrame * maxOffset) ? '#FFFFFF00' : '#FFFFFFAA' ) )
+        //.style('color', (d,i)=> (i!=0 & initialOffsets[i] <= (textFrame * maxOffset) ? '#CCCCCC' : '#A52A2A' ) )
+                    
     d3.select(".map-text#banned-abortion") 
         .style('top',(d,i)=> ( - (textFrame* maxOffset) 
                     ) * 100 + '%')
     
-
-
     if(frame> goingBlueFrame && goingBlue == false){ 
     goingBlue = true; 
     textsDiv.transition().duration(smallerScreen ? 300 : 1200).style('opacity',0)
         .on('end', () => {
         weVote.transition().duration(1200).style('opacity',1)
-        bubblesG.transition().delay(500).duration(1200).attr('fill','#00abff') 
+        bubblesG.transition().delay(500).duration(1200).attr('fill','#00abff66') 
             .on('end', () => {
-                weVote.select('button').transition().style('opacity',1)
+                weVote.select('a.btn').transition().style('opacity',1)
             })
     })
     } else if (frame <= goingBlueFrame && goingBlue == true ) { 
@@ -129,7 +168,7 @@ update = function(frame) {
     weVote.transition().duration(1200).style('opacity',0)
         .on('end', () => {
         textsDiv.transition().duration(1200).style('opacity',1)  
-        bubblesG.transition().duration(1200).attr('fill','brown') 
+        bubblesG.transition().duration(1200).attr('fill','#A52A2A66') 
         })
     }
 }
